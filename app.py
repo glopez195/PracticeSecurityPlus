0863ceaa71056f3f94983a7c1b7d1901fbf82aba
Guillermo <guilopzv@gmail.com>
2024-05-10 14:56:39 -0400

    fixed bug where last question was not counting

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import logging
 
 # Initialize Flask application
 app = Flask(__name__)
+questions = []
 
 # Configurations for template reloading and session management
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -41,43 +42,54 @@ def start_exam():
     session['score'] = None
     session['total'] = len(exam_data[selected_exam])    
     session['history'] = []
+    session['incorrect_answers'] = []
     session['answered_questions'] = None
-    return redirect(url_for('question'))
-
-# Handle display and processing of individual questions
-@app.route('/question', methods=['GET', 'POST'])
-def question():
-    selected_exam = session.get('selected_exam', None)
-    question_number = session.get('current', 0)
+    global questions
     questions = exam_data.get(selected_exam, [])
-    
-    if questions and 0 <= question_number < len(questions):
-            correct_answer = questions[question_number].get('correct_answer', '')
-
     if session.get('answered_questions', None) is None:
         session['answered_questions'] = [False] * len(questions)
 
     if session.get('score', None) is None:
         session['score'] =  [False] * len(questions)
+    return redirect(url_for('question'))
+
+# Handle display and processing of individual questions
+@app.route('/question', methods=['GET', 'POST'])
+def question():
+    question_number = session.get('current', 0)
+
+    if questions and 0 <= question_number < len(questions):
+            correct_answer = questions[question_number].get('correct_answer', '')
 
     if request.method == 'POST':
+        print(session['current'])
         action = request.form.get('action', '')
         user_answers = request.form.getlist('answer')   # Fetch all checked answers     
         
         # Check if the answers are correct
-        if user_answers == correct_answer:
-            result = "Correct"                
-        else:
-            result = "Incorrect"
+        result = "Incorrect"
 
         if user_answers:
             session['answered_questions'][question_number] = True
 
         explanation = questions[question_number].get('explanation', '')
+        # Append to history and update score if necessary
+        if user_answers:
+            if user_answers != correct_answer and question_number not in session['incorrect_answers']:
+                session['incorrect_answers'].append(question_number)
+                session['history'].append({
+                    'question': questions[question_number]['question'],
+                    'correct_answer': correct_answer,
+                    'user_answers': user_answers,
+                    'explanation': explanation
+                })
+                session['score'][question_number] = False
+            else:
+                result = "Correct"
+                session['score'][question_number] = True
 
         # Render the same question template with results if checked
-        if action == 'check':
-            session['answered_questions'][question_number] = True                        
+        if action == 'check':                        
             return render_template('question.html', question=questions[question_number],
                                    current=question_number + 1, total=len(questions),
                                    result=result, explanation=explanation, user_answers=user_answers, 
@@ -91,20 +103,6 @@ def question():
         elif action.isdigit():
             session['current'] = int(action) 
 
-        # Append to history and update score if necessary
-        if user_answers:
-            if result == "Incorrect":
-                session['history'].append({
-                    'question': questions[question_number]['question'],
-                    'correct_answer': correct_answer,
-                    'user_answers': user_answers,
-                    'explanation': explanation,
-                    'result': result
-                })
-                session['score'][question_number] = False
-            else:
-                session['score'][question_number] = True
-
         return redirect(url_for('question'))
 
     return render_template('question.html', question=questions[question_number],
@@ -112,15 +110,34 @@ def question():
                            answered_questions=session['answered_questions'])
 
 # Route to finalize the exam and display results
-@app.route('/finish', methods=['POST', 'GET'])
+@app.route('/finish', methods=['POST'])
 def finish():
-    # Calculate final score and determine pass/fail based on a threshold of 83.33% coming from Comptia Sec+ exam 750/900
-    final_score = session.get('score', []).count(True)    
-    total = session['total']
-    percentage = (final_score / total) * 100
-    if percentage >= 83.33:
-        result = "Pass"
-    else:
-        result = "Fail"
-    return render_template('result.html', result=result, score=final_score, percentage=percentage, total=total, history=session['history'])
+    if request.method == 'POST':        
+        question_number = session.get('current', 0)
+        user_answers = request.form.getlist('answer')
+        correct_answer = questions[question_number].get('correct_answer', '')
+        explanation = questions[question_number].get('explanation', '')
+
+        if user_answers:
+            if user_answers == correct_answer:
+                session['score'][question_number] = True
+            else:
+                session['incorrect_answers'].append(question_number)
+                session['history'].append({
+                    'question': questions[question_number]['question'],
+                    'correct_answer': correct_answer,
+                    'user_answers': user_answers,
+                    'explanation': explanation
+                })
+                session['score'][question_number] = False
+                
+        # Calculate final score and determine pass/fail based on a threshold of 83.33% coming from Comptia Sec+ exam 750/900
+        final_score = session.get('score', []).count(True)    
+        total = session['total']
+        percentage = (final_score / total) * 100
+        if percentage >= 83.33:
+            result = "Pass"
+        else:
+            result = "Fail"
+        return render_template('result.html', result=result, score=final_score, percentage=percentage, total=total, history=session['history'])
 
